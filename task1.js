3402a2a9cf13a06b12851d18838e712ee6621344
glzd222 <mi85h7wkd@code.edu.az>
2024-04-23 14:30:23 -0700

    change

---
+++ task1.js
@@ -0,0 +1,7 @@
+const toplama =document.querySelector(".toplama")
+const netice=document.querySelector(".netice")
+toplama.addEventListener("click",function(){
+
+return reqem1+reqem2
+    console.log(netice.value)
+})
